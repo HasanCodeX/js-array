1409c0dd51039953d3c854204b3e4e26773f2b38
HasanCodeX <dev.hasan10@gmail.com>
2025-02-10 23:55:53 +0600

    includes

---
+++ includes.js
@@ -0,0 +1,11 @@
+const friends = ['John', 'Peter', 'Bob', 'Anna', 'Oliver'];
+
+console.log(friends.includes('Bob'));
+console.log(friends.includes('Mehedi'));
+
+if (friends.includes('Bob')) {
+    console.log('You have a friend called Bob');
+}
+else {
+    console.log('You do not have a friend called Bob');
+} 
