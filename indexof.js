c04d8ab2aeb1a5bfe938d4ddda646a1535596016
HasanCodeX <dev.hasan10@gmail.com>
2025-02-11 01:29:56 +0600

    last commit

---
+++ indexof.js
@@ -0,0 +1,5 @@
+const friends = ['John', 'Peter', 'Bob', 'Anna', 'Oliver'];
+
+console.log(friends.indexOf('Bob'));
+console.log(friends.indexOf('Mehedi'));
+ 
---
+++ isarray.js
@@ -0,0 +1,10 @@
+const friends = ['John', 'Peter', 'Bob', 'Anna', 'Oliver'];
+
+const num = [];
+const food = 'rosegolla';
+const age = 25; // Fix: age variable is now declared
+
+console.log(Array.isArray(friends)); // true
+console.log(Array.isArray(num)); // true
+console.log(Array.isArray(food)); // false
+console.log(Array.isArray(age)); // false
