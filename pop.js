75390e488966453e7e58f6183eff38d65911b8bc
HasanCodeX <dev.hasan10@gmail.com>
2025-02-10 23:51:43 +0600

    explore push and pop and shift

---
+++ pop.js
@@ -0,0 +1,11 @@
+const friends = ['John', 'Peter', 'Bob', 'Anna', 'Oliver'];
+// friends.push( 'Tom', 'Jerry', 'Donald');
+const out1 = friends.pop( );
+const out2 = friends.pop( );
+friends.pop( );
+
+console.log(friends);
+
+
+
+ 
---
+++ shift-unshift.js
@@ -0,0 +1,6 @@
+const friends = ['John', 'Peter', 'Bob', 'Anna', 'Oliver'];
+
+
+friends.shift( );
+friends.shift( );
+console.log(friends);
