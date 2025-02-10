381cd34eba3fb559c5c0c5063639e4b14de73932
HasanCodeX <dev.hasan10@gmail.com>
2025-02-10 23:22:43 +0600

    explore array

---
+++ array.js
@@ -0,0 +1,9 @@
+// const num1 = 12;
+// const num2 = 23;
+// const num3 = 34;
+// const num4 = 45;
+
+const numbers = [12, 23, 34, 45];
+const friends = ['John', 'Peter', 'Bob', 'Anna', 'Oliver'];
+const passed = [true, false, true, true, false];
+const mixedArray = ['John', 23, true, 'Anna', 34, false, 'Oliver'];
