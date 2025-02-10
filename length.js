ff25068cebac03158200ed4d37f0ea25b1f97820
HasanCodeX <dev.hasan10@gmail.com>
2025-02-10 23:34:15 +0600

    explore length of array

---
+++ length.js
@@ -0,0 +1,20 @@
+// const numbers = [12, 23, 34, 45];
+// console.log(numbers.length); // 4
+
+// const numbers = [12, 23, 34, 45, 56, 67, 78, 89];
+// console.log(numbers[2]); // 4
+
+
+const numbers = [12, 23, 34, 45, 56, 67, 78, 89];
+console.log(numbers[3]); // 4
+
+
+const fourth = numbers[3];
+console.log(fourth); // 45
+
+//set ot update elements value bu index
+let abc = 45;
+abc = 67;
+
+numbers[1] = 8777;
+console.log(numbers[1]); // 8777
